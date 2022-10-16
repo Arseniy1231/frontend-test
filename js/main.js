aff0acb8fe8251d9f140ca1928b3ed2aede6d9b7
Arseniy <64345353+Arseniy1231@users.noreply.github.com>
2022-10-16 09:24:49 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,15 +15,9 @@ $(function() {
     $(".mobile-menu-list").find(".accordion-toggle").click(function() {
       $(this).next().toggleClass("open").slideToggle("fast");
       $(this).toggleClass("active-tab").find(".menu-link").toggleClass("active");
-  
-      // $(".mobile-menu-list .accordion-content").not($(this).next()).slideUp("fast").removeClass("open");
-      // $(".mobile-menu-list .accordion-toggle").not(jQuery(this)).removeClass("active-tab").find(".menu-link").removeClass("active");
     });
     $(".mobile-menu-list").find(".accordion-toggle-lv2").click(function() {
       $(this).next().toggleClass("open").slideToggle("fast");
       $(this).toggleClass("active-tab").find(".menu-link-lv2").toggleClass("active");
-  
-      // $(".mobile-menu-list .accordion-content-lv2").not($(this).next()).slideUp("fast").removeClass("open");
-      // $(".mobile-menu-list .accordion-toggle-lv2").not(jQuery(this)).removeClass("active-tab").find(".menu-link-lv2").removeClass("active");
     });
   }); 
